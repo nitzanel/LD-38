62d4b59f7f8154e88ac2ebf06ba60942534c644d
Nitzan <nitzanel2@gmail.com>
2016-12-10 14:52:44 +0200

    added Character class

--- LD-38/Assets/Models/Character.cs
+++ LD-38/Assets/Models/Character.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using UnityEngine;
 namespace AssemblyCSharp
 {
 	public class Character
@@ -22,7 +22,20 @@ namespace AssemblyCSharp
 		/// <param name="quantity">Quantity.</param>
 		public void takeItem(Inventory giver_inventory, string name, int quantity)
 		{
-			
+			if (giver_inventory.items.ContainsKey (name)) {
+				int giver_quantity = giver_inventory.items [name].quantity;
+				if (giver_quantity >= quantity) {
+					giver_inventory.addItem (name, -quantity);
+					this.inventory.addItem (name, quantity);
+				} else {
+					giver_inventory.addItem (name, -giver_quantity);
+					this.inventory.addItem (name, giver_quantity);
+				}
+			}
+			else
+			{
+				Debug.Log ("Requested item that doesnt exist");
+			}
 		}
 		/// <summary>
 		/// Takes the item.
@@ -31,7 +44,17 @@ namespace AssemblyCSharp
 		/// <param name="item">Item.</param>
 		public void takeItem(Inventory giver_inventory, Item item)
 		{
-
+			int giver_quantity = giver_inventory.items [item.name].quantity;
+			if (giver_quantity >= item.quantity)
+			{
+				giver_inventory.addItem (item.name, -item.quantity);
+				this.inventory.addItem (item.name, item.quantity);
+			}
+			else
+			{
+				giver_inventory.addItem (item.name, -giver_quantity);
+				this.inventory.addItem (item.name, giver_quantity);
+			}
 		}
 
 
