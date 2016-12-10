2bfca5eff8fd1e198ca30ca1db10ae325a3aed7f
Nitzan <nitzanel2@gmail.com>
2016-12-10 20:08:51 +0200

    Added hostile trait to enemies

--- LD-38/Assets/Models/Enemy.cs
+++ LD-38/Assets/Models/Enemy.cs
@@ -4,14 +4,16 @@ namespace AssemblyCSharp
 {
 	public class Enemy : Character
 	{
-		public Enemy (int money, int health, int damage, int level)
+		public Enemy (int money, int health, int damage, int level, bool hostile)
 		{
 			this.money = money;
 			this.health = health;
 			this.damage = damage;
 			this.level = level;
 			this.weapon = new Weapon ("basic", 10);
+			this.hostile = hostile;
 		}
+		public bool hostile;
 	}
 }
 
