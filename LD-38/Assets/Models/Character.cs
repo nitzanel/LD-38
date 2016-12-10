d1d410fa7672a360cfef86b0113f9a5e29fd23a0
Nitzan <nitzanel2@gmail.com>
2016-12-10 12:49:08 +0200

    Added base character class to inherit from

---
+++ LD-38/Assets/Models/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Character
+	{
+		public Character ()
+		{
+			this.inventory = new Inventory ();
+			this.money = 1000;
+		
+		}
+
+		public Inventory inventory;
+		public int money;
+
+
+		public void takeItems(Inventory giver_inventory, string name, int quantity)
+		{
+			
+		}
+		public void takeItems(Inventory giver_inventory, Item item)
+		{
+
+		}
+
+
+
+	}
+}
+
